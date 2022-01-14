47fdd66b55eb4786a38f71cd384016fecebdd095
Shubham1696 <shubhamsahay1696@gmail.com>
2022-01-14 15:52:53 +0530

    UI Developement Completed

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 import warnings
 warnings.filterwarnings("ignore")
+import pandas as pd
 import model
 
 
@@ -14,10 +15,13 @@ def home():
 
 @app.route('/predict',methods=['POST'])
 def predict():
-	Username = request.form.get('Username')
-	#processed_date = pre_process([data])
-	prediction = model.getOptimizedRecommendations(Username)
-	return render_template('index.html', OUTPUT=str(prediction))
+    Username = request.form.get('Username')
+    prediction = model.getOptimizedRecommendations(Username)
+    if type(prediction)==list:
+        predictionDF = pd.DataFrame(prediction, columns=['Recommended Products'])
+        return render_template('index.html', tables=[predictionDF.to_html(classes='Recommendations')], titles = ['For Username : '+ Username])
+    else:
+        return render_template('index.html', OUTPUT=prediction)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
